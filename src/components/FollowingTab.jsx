0a62d8b3274b97e9044d7a268a223f010d9eba2c
Sohail Gsais <gsais.sohail9@gmail.com>
2023-02-27 14:01:23 +0000

    fixed 401 error

--- src/components/FollowingTab.jsx
+++ src/components/FollowingTab.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { BsTwitch } from "react-icons/bs";
-import SearchField from "./SearchField";
+import React, { useState, useEffect } from 'react';
+import { BsTwitch } from 'react-icons/bs';
+import SearchField from './SearchField';
 
-const _userId = localStorage.getItem("userId");
-console.log("userId is set", _userId);
+const _userId = localStorage.getItem('userId');
+console.log('userId is set', _userId);
 function FollowingTab() {
   const [userId, setUserId] = useState(_userId);
 
@@ -17,10 +17,10 @@ function FollowingTab() {
 
   return (
     <div className="flex flex-col space-y-6 items-center justify-center">
-      {/* <LinkWithTwitch /> */}
+      <LinkWithTwitch />
       <SearchField
         onResultsClick={(channel) => {
-          localStorage.setItem("userId", channel.id);
+          localStorage.setItem('userId', channel.id);
           setUserId(channel.id);
         }}
       />
@@ -46,13 +46,13 @@ function LinkWithTwitch() {
 
 async function getFollowingChannels(userId) {
   try {
-    console.log("fetchning following channels...");
+    console.log('fetchning following channels...');
     const response = await fetch(
       `https://api.twitch.tv/helix/users/follows?first=100&from_id=${userId}`,
       {
         headers: {
-          Authorization: "Bearer 8uhzngjj8p88lo20achk8lm6rbo4p7",
-          "Client-Id": _envs.CLIENT_ID,
+          Authorization: 'Bearer 8uhzngjj8p88lo20achk8lm6rbo4p7',
+          'Client-Id': _envs.CLIENT_ID,
         },
       }
     );
--- src/popup.jsx
+++ src/popup.jsx
@@ -1,19 +1,19 @@
-import { Switch } from "@blueprintjs/core";
-import { signOut } from "firebase/auth";
-import { doc, onSnapshot } from "firebase/firestore";
-import React, { useContext, useEffect, useState } from "react";
-import { render } from "react-dom";
-import { useAuthState } from "react-firebase-hooks/auth";
-import AuthWrapper from "./components/AuthWrapper";
-import Channels from "./components/Channels";
-import ContentTabs from "./components/ContentTabs";
-import SearchField from "./components/SearchField";
-import { TwitchAPI } from "./TwitchApi";
-import VodsTab from "./components/VodsTab";
-import { AppContext, AppProvider } from "./context/AppContext";
-import { auth, db, removeChannelsFromDb } from "./firebase";
-import "./tailwind.css";
-import { LayoutGrids } from "./components/LayoutGrids";
+import { Switch } from '@blueprintjs/core';
+import { signOut } from 'firebase/auth';
+import { doc, onSnapshot } from 'firebase/firestore';
+import React, { useContext, useEffect, useState } from 'react';
+import { render } from 'react-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import AuthWrapper from './components/AuthWrapper';
+import Channels from './components/Channels';
+import ContentTabs from './components/ContentTabs';
+import SearchField from './components/SearchField';
+import { TwitchAPI } from './TwitchApi';
+import VodsTab from './components/VodsTab';
+import { AppContext, AppProvider } from './context/AppContext';
+import { auth, db, removeChannelsFromDb } from './firebase';
+import './tailwind.css';
+import { LayoutGrids } from './components/LayoutGrids';
 
 const twitchApi = new TwitchAPI();
 
@@ -21,15 +21,15 @@ const twitchApi = new TwitchAPI();
 //   console.log(response.message);
 // });
 
-const vodId = new URLSearchParams(window.location.search).get("id");
+const vodId = new URLSearchParams(window.location.search).get('id');
 
 function Popup() {
-  const [selectedPage, setSelectedPage] = useState("popup");
+  const [selectedPage, setSelectedPage] = useState('popup');
   const [user, setUser] = useState();
 
   function renderPage(page) {
     switch (page) {
-      case "popup":
+      case 'popup':
         return <PopupPage />;
     }
   }
@@ -41,7 +41,7 @@ function Popup() {
   );
 }
 
-render(<Popup />, document.getElementById("root"));
+render(<Popup />, document.getElementById('root'));
 
 function SavedPanel({ loading, liveChannels }) {
   const [hideOffline, setHideOffline] = useState(true);
@@ -52,12 +52,12 @@ function SavedPanel({ loading, liveChannels }) {
 
   async function handleRemove() {
     await removeChannelsFromDb(user.uid, removeBatch);
-    updateRemoveBatch({ action: "clear" });
+    updateRemoveBatch({ action: 'clear' });
   }
 
   useEffect(() => {
     if (hideCheckBox === true) {
-      updateRemoveBatch({ action: "clear" });
+      updateRemoveBatch({ action: 'clear' });
     }
   }, [hideCheckBox]);
 
@@ -68,26 +68,23 @@ function SavedPanel({ loading, liveChannels }) {
         <Switch
           className="ml-4 text-gray-200 font-semibold outline-none mb-0"
           checked={hideOffline}
-          label={!hideOffline ? "Hide offline" : "Show offline"}
+          label={!hideOffline ? 'Hide offline' : 'Show offline'}
           onChange={() => setHideOffline(!hideOffline)}
         />
         <Switch
           className="ml-4 text-gray-200 font-semibold outline-none mb-0"
           checked={hideCheckBox}
-          label={"Remove"}
+          label={'Remove'}
           onChange={() => setHideCheckbox(!hideCheckBox)}
         />
-        <LayoutGrids
-          selectedLayout={selectedLayout}
-          setSelectedLayout={setSelectedLayout}
-        />
+        <LayoutGrids selectedLayout={selectedLayout} setSelectedLayout={setSelectedLayout} />
       </div>
       {removeBatch.length > 0 && (
         <button
           onClick={handleRemove}
           className="bg-red-500 text-white rounded shadow px-2 py-1 m-2"
         >
-          Remove selected{" "}
+          Remove selected{' '}
         </button>
       )}
       {/* {loading && <LoadingSpinner text={"updating..."} />} */}
@@ -110,7 +107,7 @@ function SavedPanel({ loading, liveChannels }) {
 }
 
 export async function getLiveChannels(channels) {
-  console.log("fetching live channels : ", channels);
+  console.log('fetching live channels : ', channels);
   // const bearer = await twitchApi.getToken();
   const liveChannels = await Promise.all(
     channels.map(async (channel) => {
@@ -158,8 +155,8 @@ function PopupPage() {
   }, [user, savedChannels]);
 
   useEffect(() => {
-    const unsub = onSnapshot(doc(db, "channels", user.uid), (doc) => {
-      console.log("Current data: ", doc.data());
+    const unsub = onSnapshot(doc(db, 'channels', user.uid), (doc) => {
+      console.log('Current data: ', doc.data());
       setSavedChannels(doc.data().savedChannels);
     });
 
@@ -183,8 +180,8 @@ function PopupPage() {
       <ContentTabs
         tabs={[
           {
-            id: "saved",
-            title: "Saved",
+            id: 'saved',
+            title: 'Saved',
             panel: <SavedPanel liveChannels={liveChannels} loading={loading} />,
           },
           // {
@@ -194,8 +191,8 @@ function PopupPage() {
           //   disabled: false,
           // },
           {
-            id: "vods",
-            title: "Vods",
+            id: 'vods',
+            title: 'Vods',
             panel: <VodsTab />,
             disabled: !selectedUuid,
           },
